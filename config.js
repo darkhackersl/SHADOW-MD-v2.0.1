369f26849d8ad16ae2163c68499f44bfbbfd71c7
teclogishadow <vipshadowmdv@gmail.com>
2024-09-09 14:29:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put session id",
+SESSION_ID: process.env.SESSION_ID || "BYoX3Rob#3R59BfbBV1f8qw7Fz22f3tLMXfCUyPIGEs-ENqyBuzo",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
 SUDO_NB: process.env.SUDO_NB || "94767910958",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
